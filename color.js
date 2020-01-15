00b58a11bdbf83cf6c8db259fc207bd76d0f6e35
Maja Polanc <mayainstudio@gmail.com>
2020-01-15 22:13:24 +0100

    logic for generating random colors, displaying them in rgb format, turning squares into a winning color if you win, displayin message if you've lost/won

--- color.js
+++ color.js
@@ -1,37 +1,23 @@
-let colors = [
-  "rgb (255, 0, 0)",
-  "rgb (255, 255, 0)",
-  "rgb (255, 255, 255)",
-  "rgb (0, 0, 255)",
-  "rgb (0, 255, 255)",
-  "rgb (255, 0, 255)",
-  ]
-
 let squares = document.querySelectorAll(".square");
+let colors = generateRandomColors(6); // generate colors for 6 squares
+// let colors = [
+//   "rgb (255, 0, 0)",
+//   "rgb (255, 255, 0)",
+//   "rgb (255, 255, 255)",
+//   "rgb (0, 0, 255)",
+//   "rgb (0, 255, 255)",
+//   "rgb (255, 0, 255)",
+//   ]
+let pickedColor = pickColor();
+let colorDisplay = document.getElementById("display");
+let messageDisplay = document.querySelector("#message");
+
+//will change colors of squares
 for (let i = 0; i < squares.length; i ++){
   squares[i].style.backgroundColor = colors[i]
 }
 
-// let colors = generateRandomColors(6);
-function generateRandomColors(){
-  let array = [];
-  for(let i = 0; i < squares.length; i ++){
-    let r = Math.floor(Math.random()*256);
-    let g = Math.floor(Math.random()*256);
-    let b = Math.floor(Math.random()*256);
-    return "rgb ("+ r +" , "+ g +" , " + b + ")";
-  }
-}
-
-
-
-let pickedColor = "";
-function pickColor(){ // pick random colors
-  let random = Math.floor(Math.random()*colors.length);
-  return colors[random];
-}
-
-let colorDisplay = document.getElementById("display");
+// if you win/lose message display
 colorDisplay.textContent = pickedColor;
 for(let i = 0; i < squares.length; i ++){
   squares[i].style.backgroundColor = colors[i];
@@ -40,19 +26,41 @@ for(let i = 0; i < squares.length; i ++){
       if (clickedColor === pickedColor){
         alert("correct!");}
       else {this.style.backgroundColor = "#232323"} //black
-
-    let messageDisplay = document.querySelector("#message");
       if(clickedColor === pickedColor){
       messageDisplay.textContent = "correct"
       }
       else {
       messageDisplay.textContent = "Try again"
       }
-    })
+    });
   }
 
-function changeColors(color){ //change squares into a color that is a winning color
+//if you win, change square's colors into a color that is a winning color
+function changeColors(color){
   for(let i = 0; i < squares.length; i ++){
     squares[i].backgroundColor = color;
   }
+}
+
+// pick random colors
+function pickColor(){
+  let random = Math.floor(Math.random()*colors.length);
+  return colors[random]; // extracting from array
+}
+
+//display random color in rgb format
+function randomColor(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return "rgb ("+ r +" , "+ g +" , " + b + ")";
+  }
+
+//generate random colors for squares
+function generateRandomColors(){
+  let array = [];
+  for(let i = 0; i < squares.length; i ++){
+    array.push(randomColor())
+  }
+  return array;
 }
